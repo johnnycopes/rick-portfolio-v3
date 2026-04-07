84eefe80815ee379caa4c7ca8b8037a1c20eb8c0
John Coppola <jdcoppola@gmail.com>
2026-04-07 14:30:28 -0400

    FEATURE: deletes home page and reroutes to work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,5 @@
-import styles from "@/styles/pages/index.module.scss"
-import Layout from "@/components/Layout"
-import Name from "@/components/Name"
-import Button from "@/components/Button"
-import InternalLink from "@/components/InternalLink"
+import { redirect } from "next/navigation"
 
-const IndexPage = () => (
-  <Layout
-    showHeader={false}
-    verticallyCentered={true}
-    horizontallyCentered={true}
-    >
-    <h1 className={styles.name}>
-      <Name />
-    </h1>
-    <div className={styles.links}>
-      <InternalLink className={styles.link}
-        link="/work"
-        >
-        <Button>
-          work
-        </Button>
-      </InternalLink>
-      <InternalLink className={styles.link}
-        link="/about"
-        >
-        <Button>
-          about
-        </Button>
-      </InternalLink>
-      <InternalLink className={styles.link}
-        link="/misc"
-        >
-        <Button>
-          misc
-        </Button>
-      </InternalLink>
-    </div>
-  </Layout>
-)
+const IndexPage = () => redirect("/work")
 
 export default IndexPage
