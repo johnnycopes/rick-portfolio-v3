bcbb67cf6be5ac258739c59f4b3dc5a41a64311d
John Coppola <jdcoppola@gmail.com>
2026-04-08 07:55:09 -0400

    FEATURE: prefetches CSS exclusively on <Link> hover

--- src/components/InternalLink.tsx
+++ src/components/InternalLink.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useState } from "react"
 import cx from "classnames"
 
 import styles from "@/styles/components/Link.module.scss"
@@ -16,11 +17,15 @@ interface InternalLinkProps {
 const InternalLink = ({ link, className, applyActiveClass, children }: InternalLinkProps) => {
   const pathname = usePathname()
   const isActive = applyActiveClass && pathname.startsWith(link)
+  const [hovered, setHovered] = useState(false)
 
   return (
-    <Link className={cx(styles.link, className, { [styles.active]: isActive })}
+    <Link
+      className={cx(styles.link, className, { [styles.active]: isActive })}
       href={link}
-      >
+      prefetch={hovered ? null : false}
+      onMouseEnter={() => setHovered(true)}
+    >
       {children}
     </Link>
   )
